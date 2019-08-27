860b0baf20b9c56610bb85b5d9a13d008e37c2ca
yanjifa <yanjifa@joycastle.mobi>
2019-08-27 10:15:25 +0800

    commit

---
+++ assets/Script/CircleAvatar.ts
@@ -0,0 +1,23 @@
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class CircleAvatar extends cc.Component {
+
+    private time: number;
+
+    protected onLoad() {
+        // 关闭动态和图, 否则浏览器显示不正确
+        cc.dynamicAtlasManager.enabled = false;
+        this.time = 0;
+    }
+
+    protected update(dt: number) {
+        this.time += dt;
+        const edge = Math.abs(Math.sin(this.time)) / 2;
+        const sprite = this.node.getComponent(cc.Sprite);
+        const material = sprite.sharedMaterials[0];
+        material.setProperty("u_edge", edge);
+        sprite.setMaterial(0, material);
+    }
+}
