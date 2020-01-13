f6e626b3ca5b4a07fac7a6957134e5ed8e208a94
yanjifa <yanjifa@joycastle.mobi>
2020-01-13 14:36:47 +0800

    :arrow_up: 升级至 2.3.0

--- assets/Script/CircleAvatar.ts
+++ assets/Script/CircleAvatar.ts
@@ -1,15 +1,42 @@
 
 const {ccclass, property} = cc._decorator;
 
+interface NodeAttr {
+    /** 缩放 */
+    scale: number,
+    /** 角度 */
+    angle: number,
+    /** x 位置 */
+    x: number,
+    /** y 位置 */
+    y: number,
+    /** 位置 */
+    position: cc.Vec2 | cc.Vec3,
+    /** 透明度 */
+    opactiy: number,
+}
+
 @ccclass
 export default class CircleAvatar extends cc.Component {
 
     private time: number;
 
     protected onLoad() {
-        // 关闭动态和图, 否则浏览器显示不正确
-        cc.dynamicAtlasManager.enabled = false;
         this.time = 0;
+    }
+
+    protected start() {
+        // cc.tween(this.node)
+        //     .repeatForever(
+        //         cc.tween().to(1, { scale: 2, angle: -360 } as NodeAttr)
+        //             .call(() => { console.log("tween action callback") })
+        //             .to(1, { scale: 1.0, angle: 0 } as NodeAttr)
+        //     )
+        //     .start()
+        // this.scheduleOnce(() => {
+        //     console.log("stopAllActions");
+        //     this.node.stopAllActions();
+        // }, 5);
     }
 
     protected update(dt: number) {
@@ -17,9 +44,7 @@ export default class CircleAvatar extends cc.Component {
         const edge = Math.abs(Math.sin(this.time)) / 2;
         const sprite = this.node.getComponent(cc.Sprite);
         const material = sprite.getMaterial(0);
-        if (material) {
-            material.setProperty("u_edge", edge);
-            sprite.setMaterial(0, material);
-        }
+        material.setProperty("edge", edge);
+        sprite.setMaterial(0, material);
     }
 }
